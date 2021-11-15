13d86273c024601f00389c3afe4bbcdfec483ca9
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2021-11-15 15:24:21 -0700

    added descibe tests

--- functions.js
+++ functions.js
@@ -7,5 +7,14 @@ module.exports = {
     },
     add: (num1, num2) => {
         return num1 + num2
+    },
+    multiply: (num1, num2) => {
+        return num1 * num2
+    },
+    divide: (num1, num2) => {
+        return num1 / num2
+    },
+    subtract: (num1, num2) => {
+        return num1 - num2
     }
 }
--- functions.test.js
+++ functions.test.js
@@ -2,25 +2,46 @@ const { test, expect } = require('@jest/globals')
 const {
     returnTwo,
     greeting,
-    add
+    add,
+    multiply,
+    divide,
+    subtract
+
 } = require('./functions')
 
-test("will test returnTwo", () => {
-  expect(returnTwo()).toEqual(2)
+// test("will test returnTwo", () => {
+//   expect(returnTwo()).toEqual(2)
+// });
+
+// test("will test greeting's return", () => {
+//     expect(greeting('Tanner')).toEqual("Hello, Tanner.")
+// });
+
+// test("will test greeting's return", () => {
+//     expect(greeting('Josh')).toEqual("Hello, Josh.")
+// });
+
+
+// test("will test the sum of add", () => {
+    //     expect(add(5, 9)).toEqual(14)
+    // });
+    
+    describe("math function ", ()=> {
+    test("will test the sum of add", () => {
+        expect(add(1, 2)).toEqual(3)
+    });
+    
+    test("will test the multiplication of multiply", () => {
+        expect(multiply(1, 2)).toEqual(2)
+    });
+    
+    test("will test the division of divide", () => {
+        expect(divide(6, 2)).toEqual(3)
+    });
+    
+    test("will test the subtraction of subtract", () => {
+        expect(subtract(1, 2)).toEqual(-1)
+    });
 });
 
-test("will test greeting's return", () => {
-    expect(greeting('Tanner')).toEqual("Hello, Tanner.")
-});
 
-test("will test greeting's return", () => {
-    expect(greeting('Josh')).toEqual("Hello, Josh.")
-});
-
-test("will test the sum of add", () => {
-    expect(add(1, 2)).toEqual(3)
-});
-
-test("will test the sum of add", () => {
-    expect(add(5, 9)).toEqual(14)
-})
